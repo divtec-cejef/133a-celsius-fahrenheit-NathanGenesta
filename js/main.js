dc6fd68fca71a3ef88528e49e702ad3942571947
genenat <nathan.genesta@divtec.ch>
2023-09-05 15:02:05 +0200

    exercice fini

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
+ * @author  Nathan Génesta
  * @version 0.2
- * @since   2019-08-19
+ * @since   2023-09-05
  */
 'use strict'; // Demande un interprétation stricte du code
 
-let a = 'Bonjour';
-
-console.log(a);
+// Demande la température
+let temperature = prompt("Température en celcius :");
+// Transforme age en entier
+temperature = parseInt(temperature);
+// Si l'âge n'est pas un nombre => erreur
+if(isNaN(temperature)) {
+    alert("Entrer une température !");
+} else {
+    // Affiche la température en farhenheit
+    alert(`${temperature}°C = ${temperature * 9 / 5 + 32}°F`);
+}
